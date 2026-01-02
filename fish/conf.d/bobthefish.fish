cb2489252defef9afb178c7f78026701a116a328
hi-masaki <h69.masaki@gmail.com>
2026-01-02 14:35:11 +0900

    first

---
+++ fish/conf.d/bobthefish.fish
@@ -0,0 +1,7 @@
+# viモードの表示
+set -g theme_display_vi yes
+# 終了コードの表示 
+set -g theme_show_exit_status yes
+# ユーザ名の表示
+set -g theme_display_user yes
+
---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# viモードの設定
+fish_vi_key_bindings
